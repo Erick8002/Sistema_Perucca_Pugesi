095302e0be008cdd7e27be0e2a6421dac64c7e5b
Erick <eshinfuku@gmail.com>
2026-08-13 10:19:23 -0300

    style: add company logo to Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import logoImg from '../assets/Logo.png';
 
 export default function Sidebar() {
   return (
-    <aside className="w-20 bg-white border-r border-gray-200 flex flex-col items-center py-6 shrink-0">
-      <div className="w-12 h-16 border-2 border-black flex items-center justify-center font-bold text-2xl tracking-tighter">
-        <span className="text-black">P</span>
-        <span className="text-gray-500">d</span>
-      </div>
+    <aside className="w-36 bg-white border-r border-gray-200 flex flex-col items-center py-6 shrink-0">
+      <img src={logoImg} alt="Logo" className="w-28"/>
+      
     </aside>
   );
 }
